c239099fd096ab381ceed6547835d02e44fc8f4f
Vítor Soares Vian <vitorsoaresvian@gmail.com>
2017-10-11 21:19:13 -0300

    Update mysimpletop.c

--- mysimpletop.c
+++ mysimpletop.c
@@ -1,3 +1,27 @@
+int GetRamInKB(void)
+{
+    FILE *meminfo = fopen("/proc/meminfo", "r");
+    if(meminfo == NULL)
+        ... // handle error
+
+    char line[256];
+    while(fgets(line, sizeof(line), meminfo))
+    {
+        int ram;
+        if(sscanf(line, "MemTotal: %d kB", &ram) == 1)
+        {
+            fclose(meminfo);
+            return ram;
+        }
+    }
+
+    // If we got here, then we couldn't find the proper line in the meminfo file:
+    // do something appropriate like return an error code, throw an exception, etc.
+    fclose(meminfo);
+    return -1;
+}
+
+
 cat /proc/meminfo | awk '{print $2}' | head -1
 #include <pthread.h>
 #include <stdio.h>
